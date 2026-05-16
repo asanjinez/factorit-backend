1e72d52aff3b4bfcd0a489ed244775f97f544c85
asanjinez <alejandro.sanjinez1@gmail.com>
2026-05-16 08:18:35 -0300

    spring project started

---
+++ src/main/java/com/factorit/FactoritApplication.java
@@ -0,0 +1,13 @@
+package com.factorit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoritApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factorit/FactoritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factorit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
